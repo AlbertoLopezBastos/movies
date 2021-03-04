ed513fd81edad694aa3960a800ed9b5369ef744d
Alberto <elbeto182@hotmail.com>
2021-03-03 23:27:13 -0300

    fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => (
     <div className="header">
         <img src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_long_1-8ba2ac31f354005783fab473602c34c3f4fd207150182061e425d366e4f34596.svg" alt="" />
-    </div>
+    </div> 
 );
 
 export default Header;
