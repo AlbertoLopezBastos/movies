d0139f119766837b5e40683e89ccd9d526601295
Alberto <elbeto182@hotmail.com>
2021-03-07 17:09:29 -0300

    change for deploy

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,15 +1,12 @@
 import React from 'react';
 
 class SearchBox extends React.Component {
-
-
   onClick = () => {
     let searchValue = document.getElementById('searchMovie').value;
     this.props.handleSearch(searchValue);
   }
 
   render(){
-
     return (
       <div className="searchBox">
         <input className="searchBox__input" placeholder="Search Movie" type="search" name="searchMovie" id="searchMovie"/>
